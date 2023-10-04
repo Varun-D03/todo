862aa8a850563306af81d4b0ac0338a767711287
Varun-D03 <varundaruna@gmail.com>
2023-10-04 20:58:57 +0530

    initial

---
+++ server/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+   url: "mongodb+srv://yashdaga7019:KhHVuE9vk9xy5Zwu@cluster0.qk5xwfv.mongodb.net/varun"
+
+ // url:"mongodb+srv://varundacharya34:<VS5PJhI5TQb6gCWR>@book.xok3eaw.mongodb.net/book"
+  
+};
